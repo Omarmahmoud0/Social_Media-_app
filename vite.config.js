4f2f86426e7937f01d54840422a02e25d3e8f1ea
Omarmahmoud0 <omarmahmoud@gmail.com>
2024-07-21 03:42:46 +0300

    added index.html file

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+// const nextConfig = {
+//   async headers() {
+//     return [
+//       {
+//         source: "/(.*)",
+//         headers: [
+//           {
+//             key: "Cross-Origin-Opener-Policy",
+//             value: "same-origin",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+// nextConfig()
